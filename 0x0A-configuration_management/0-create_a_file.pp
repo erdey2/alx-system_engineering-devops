7ecca4b683c234f41e1d99688663a512d853a9da
Erdey Syoum <erdeysyoum@gmail.com>
2024-03-22 02:47:21 -0700

    configuration management with puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,8 @@
 # create a new file with a given permissions
 
-
 file { '/tmp/school':
-	owner => 'www-data',
-	group => 'www-data',
-	mode => '0744',
-	content =>'I love Puppet',
+  owner => 'www-data',
+  group => 'www-data',
+  mode => '0744',
+  content =>'I love Puppet'
 }
