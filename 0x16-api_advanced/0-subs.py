88a66f1c4ee50534b7c182b899f89268121fb841
erdey syoum <erdeysyoum@gmail.com>
2024-05-07 03:41:06 -0700

    advanced api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
 """extract data from api endpoints"""
+import requests
 
 
 def number_of_subscribers(subreddit):
     """ find the number of subscribers """
-    import requests
-
-    data = requests.get("https://www.reddit.com/r/{}/about.json"
-                        .format(subreddit),
-                        headers={"User-Agent": "My-User-Agent"},
-                        allow_redirects=False)
-    if data.status_code >= 300:
+    url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'My-User-Agent'}
+    response = requests.get(url, headers=headers)
+    if (not response.ok):
         return 0
-
-    return data.json().get("data").get("subscribers")
+    count = response.json().get('data').get('subscribers')
+    return count
