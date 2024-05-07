3e3b78d2527f0b5611799a1da76b61e4c08b9cc4
erdey syoum <erdeysyoum@gmail.com>
2024-05-07 03:12:26 -0700

    advanced api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,9 +7,9 @@ def number_of_subscribers(subreddit):
     import requests
 
     data = requests.get("https://www.reddit.com/r/{}/about.json"
-                            .format(subreddit),
-                            headers={"User-Agent": "My-User-Agent"},
-                            allow_redirects=False)
+                        .format(subreddit),
+                        headers={"User-Agent": "My-User-Agent"},
+                        allow_redirects=False)
     if data.status_code >= 300:
         return 0
 
